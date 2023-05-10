72b6431ffdaf2910c3e791ba04f42563dd6f6c9b
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-10 13:37:16 -0700

    adding connect message and snake name

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", ()=>{
+    console.log("Successfully connected to game server");
+    conn.write("Name: YER");
+  });
+
   conn.on("data", (data) => {
     console.log("Incoming data:", data);
     // process the incoming data here
