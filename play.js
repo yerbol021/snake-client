6fc43f64f47c8a2aadb48d756ee100f8f90a6885
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-10 12:33:18 -0700

    first step on snake

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Incoming data:", data);
+    // process the incoming data here
+  });
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
