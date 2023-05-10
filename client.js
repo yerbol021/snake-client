4fc0532c6a3bc9106c112b890ada22eaab3bba0f
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-10 12:57:12 -0700

    separated function from play.js file

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Incoming data:", data);
+    // process the incoming data here
+  });
+  
+  return conn;
+};
+
+module.exports = connect ;
--- play.js
+++ play.js
@@ -1,22 +1,23 @@
-const net = require("net");
+// const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log("Incoming data:", data);
-    // process the incoming data here
-  });
+//   conn.on("data", (data) => {
+//     console.log("Incoming data:", data);
+//     // process the incoming data here
+//   });
   
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
