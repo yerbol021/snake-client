2a021f3a4903e0cba53095673694180f72634524
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-10 13:48:59 -0700

    adding moves

--- client.js
+++ client.js
@@ -10,7 +10,15 @@ const connect = function () {
 
   conn.on("connect", ()=>{
     console.log("Successfully connected to game server");
-    conn.write("Name: YER");
+    Great work! Now, the truth is that we don't want our snake to automatically move up one square each time we connect.
+
+    Instruction
+    Comment out or remove the "hard-coded" Move: up message to the server.
+    
+    Conclusion
+    We're making progress! After learning the move command, we experimented with it in order to better understand the game server.
+    
+    
   });
 
   conn.on("data", (data) => {
