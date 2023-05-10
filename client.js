cbcedefb73c878b5130432b415c1967162ced883
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-10 14:27:50 -0700

    added control C key

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
@@ -8,25 +8,38 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", ()=>{
+  conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    Great work! Now, the truth is that we don't want our snake to automatically move up one square each time we connect.
-
-    Instruction
-    Comment out or remove the "hard-coded" Move: up message to the server.
-    
-    Conclusion
-    We're making progress! After learning the move command, we experimented with it in order to better understand the game server.
-    
-    
+    // conn.write("Name: YER");
+    // conn.write("Move: up");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: right");
   });
+
+  const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    };
+    // your code here
+  };
+
+  conn.on("data", handleUserInput);
 
   conn.on("data", (data) => {
     console.log("Incoming data:", data);
     // process the incoming data here
   });
-  
+
   return conn;
 };
 
-module.exports = connect ;
+module.exports = connect;
